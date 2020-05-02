a9bf0921f98ba8ac64c1506fdef6639e6d9f17b7
nolmoonen <nolmoonen@hotmail.nl>
2020-05-02 17:54:38 +0200

    added refractions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![splash](images/splash.png)
 
-An exercise into ray tracing, having Blinn-Phong shading, shadows, and reflections.
+An exercise into ray tracing containing: Blinn-Phong shading, shadows, reflections, and refractions.
 #### Building
 * Clone [stb](https://github.com/nothings/stb) into directory `external/stb`.
 * Build using CMake.
--- src/main.c
+++ src/main.c
@@ -9,15 +9,32 @@
 #include "nm_math.h"
 
 typedef struct {
-    vec3f c; // center
-    float r; // radius
+    vec3f center;
+    float radius;
 } sphere_t;
+
+typedef enum {
+    NONE, REFLECTIVE, REFRACTIVE, REFLECTIVE_REFRACTIVE
+} reflect_type_t;
+
+typedef struct {
+    reflect_type_t type;
+    union {
+        // REFLECTIVE: in [0,1], fraction of light reflected
+        float reflectiveness;
+        // REFRACTIVE: in [0,1], fraction of light refracted
+        float refractiveness;
+        // REFLECTIVE_REFRACTIVE: value is not used
+    } fraction;
+    // value not used for REFLECTIVE
+    float refractive_index;
+} reflection_t;
 
 typedef struct {
     sphere_t sphere;
     vec3f color;
     float shininess;
-    float reflectiveness;
+    reflection_t reflection;
 } ball_t;
 
 // only use to write to file
@@ -50,7 +67,7 @@ typedef struct {
  * {result} contains the reflection ray, {t} contains the distance between ray origin and intersection */
 bool reflect(ray_t *result, float *t, ray_t ray, sphere_t sphere);
 
-float compute_lighting(ray_t origin, ray_t reflected, float shininess);
+float compute_lighting(ray_t origin, vec3f normal, ray_t reflected, float shininess);
 
 /**
  * returns whether {origin} intersects with a ball in BALLS, if so:
@@ -70,17 +87,36 @@ const light_t LIGHTS[] = {
 };
 
 const ball_t BALLS[] = {
-        // couple of same sized balls
-        {.sphere={.c={-9.f, 1.f, 30.f}, .r=4.f}, .color={1.f, 0.f, 0.f},
-                .shininess=10.f, .reflectiveness=.2f},   // red
-        {.sphere={.c={-3.f, 1.f, 26.f}, .r=4.f}, .color={0.f, 1.f, 0.f},
-                .shininess=100.f, .reflectiveness=.3f},  // green
-        {.sphere={.c={+3.f, 1.f, 22.f}, .r=4.f}, .color={0.f, 0.f, 1.f},
-                .shininess=500.f, .reflectiveness=.4f},  // blue
-        {.sphere={.c={+9.f, 1.f, 18.f}, .r=4.f}, .color={1.f, 1.f, 0.f},
-                .shininess=1000.f, .reflectiveness=.5f}, // yellow
+        // red ball
+        {
+                .sphere={.center={-9.f, 1.f, 30.f}, .radius=4.f}, .color={1.f, 0.f, 0.f}, .shininess=10.f,
+                .reflection={.type=NONE}
+        },
+        // green ball
+        {
+                .sphere={.center={-3.f, 1.f, 26.f}, .radius=4.f}, .color={0.f, 1.f, 0.f}, .shininess=100.f,
+                .reflection={.type=REFLECTIVE, .fraction.reflectiveness=.2f}
+        },
+        // blue ball
+        {
+                .sphere={.center={+3.f, 1.f, 22.f}, .radius=4.f}, .color = {0.f, 0.f, 1.f}, .shininess = 500.f,
+                .reflection={.type=REFRACTIVE, .fraction.refractiveness=.9f, .refractive_index=1.5f}
+        },
+        // yellow ball
+        {
+                .sphere={.center={+9.f, 1.f, 18.f}, .radius=4.f}, .color = {1.f, 1.f, 0.f}, .shininess = 1000.f,
+                .reflection={.type=REFRACTIVE, .fraction.refractiveness=.9f, .refractive_index=1.5f}
+        },
+//        // pink ball
+//        {
+//                .sphere={.center={+14.f, 1.f, 30.f}, .radius=1.f}, .color = {1.f, 0.f, 1.f}, .shininess = 0.f,
+//                .reflection={.type=NONE}
+//        },
         // a large ball that serves as a surface
-        {.sphere={.c={0.f, -50002.5f, 24.f}, .r=50000.f}, .color={.9f, .9f, .9f}, .shininess=0.f},
+        {
+                .sphere={.center={0.f, -50002.5f, 24.f}, .radius=50000.f}, .color = {.9f, .9f, .9f}, .shininess = 0.f,
+                .reflection={.type=NONE}
+        },
         // DEBUG: render a ball in the location of a light
 //        {.sphere={.c=POINT_LIGHT_POS, .r=1.f}, .color={1.f, 1.f, 1.f}, .shininess=0.f}
 };
@@ -97,7 +133,7 @@ int main()
     const vec3f T = {0.f, 1.f, 1.f}; // target
     const vec3f W = {0.f, 1.f, 0.f}; // up-vector
     const float THETA = M_PI / 2.f;  // field of view
-    const uint32_t DEPTH = 3;
+    const uint32_t DEPTH = 8;
     const float T_MIN = 0.f;
     const float T_MAX = FLT_MAX;
 
@@ -146,8 +182,8 @@ int main()
 
 bool reflect(ray_t *result, float *t, ray_t ray, sphere_t sphere)
 {
-    vec3f v = vec3f_sub(ray.start, sphere.c);
-    float discriminant = powf(vec3f_dot(v, ray.direction), 2) - (vec3f_dot(v, v) - powf(sphere.r, 2));
+    vec3f v = vec3f_sub(ray.start, sphere.center);
+    float discriminant = powf(vec3f_dot(v, ray.direction), 2) - (vec3f_dot(v, v) - powf(sphere.radius, 2));
 
     if (discriminant < 0) {
         // quantity under the square root is negative, no intersection
@@ -170,10 +206,10 @@ bool reflect(ray_t *result, float *t, ray_t ray, sphere_t sphere)
     vec3f y = vec3f_add(ray.start, vec3f_scale(ray.direction, *t));
 
     // normal to the sphere
-    vec3f n = vec3f_norm(vec3f_sub(y, sphere.c));
+    vec3f n = vec3f_norm(vec3f_sub(y, sphere.center));
 
     // reflection direction
-    vec3f r = vec3f_sub(ray.direction, vec3f_scale(n, 2.f * vec3f_dot(n, ray.direction)));
+    vec3f r = vec3f_norm(vec3f_sub(ray.direction, vec3f_scale(n, 2.f * vec3f_dot(n, ray.direction))));
 
     result->start = y;
     result->direction = r;
@@ -181,7 +217,7 @@ bool reflect(ray_t *result, float *t, ray_t ray, sphere_t sphere)
     return true;
 }
 
-float compute_lighting(ray_t origin, ray_t reflected, float shininess)
+float compute_lighting(ray_t origin, vec3f normal, ray_t reflected, float shininess)
 {
     float intensity = 0.f;
 
@@ -197,26 +233,24 @@ float compute_lighting(ray_t origin, ray_t reflected, float shininess)
         vec3f l; // direction to the light
         float t_max = FLT_MAX;
         if (light->type == LIGHT_POINT) {
-            l = vec3f_sub(light->vec.location, reflected.start);
+            l = vec3f_norm(vec3f_sub(light->vec.location, reflected.start));
             t_max = 1.f; // prevent creating shadows beyond the light source
         } else { // if (light->type == LIGHT_DIRECTION)
-            l = light->vec.direction;
+            l = vec3f_norm(light->vec.direction);
         }
-
-        vec3f n = vec3f_norm(reflected.direction); // surface normal
-        l = vec3f_norm(l);
 
         ray_t r;
         ball_t b;
-        ray_t intersection_to_light = {reflected.start, l};
+        ray_t intersection_to_light = {.start=reflected.start, .direction=l};
         // prevent casting shadow on itself
+        // todo get all balls, and use transparency to calculate shadow coefficient
         if (get_closest_ball(&r, &b, intersection_to_light, T_CLOSE, t_max)) {
             // if a ball is in between intersection and light, this is a shadow
             continue;
         }
 
         // diffuse
-        float ln = vec3f_dot(l, n);
+        float ln = vec3f_dot(l, normal);
         if (ln > 0.f) {
             intensity += light->intensity * ln;
         }
@@ -260,31 +294,85 @@ bool get_closest_ball(ray_t *reflected, ball_t *ball, ray_t origin, float t_min,
 
 vec3f trace_ray(ray_t ray, uint32_t depth, float t_min, float t_max)
 {
-    ray_t reflection;
-    ball_t closest_ball;
+    ball_t closest_ball; // ball that has the closest intersection with the ray
+    ray_t reflection;    // ray reflecting of that ball
     bool intersect = get_closest_ball(&reflection, &closest_ball, ray, t_min, t_max);
 
     if (!intersect) {
+        // if the ray does not hit an object, use the background color
         const vec3f BACKGROUND = {.1f, .1f, .1f};
         return BACKGROUND;
     }
 
-    // get the color of the first intersection
-    vec3f color = vec3f_scale(closest_ball.color, compute_lighting(ray, reflection, closest_ball.shininess));
-    // todo bounce {reflection} again to more objects
+    // normal outwards from the sphere center
+    vec3f normal = vec3f_norm(vec3f_sub(reflection.start, closest_ball.sphere.center));
+    // get the color of the intersection with the closest ball
+    float intensity = compute_lighting(ray, normal, reflection, closest_ball.shininess);
+    vec3f color = vec3f_scale(closest_ball.color, intensity);
 
-    // if not reflective, or max depth is reached, do not
-    if (closest_ball.reflectiveness <= 0 || depth == 0) {
+    if (depth == 0) {
+        // if max depth is reached, do not reflect or refract ray
         return color;
     }
 
-    // reflect ray
-    vec3f reflected_color = trace_ray(reflection, depth - 1, T_CLOSE, t_max);
+    // handle reflection and refraction
+    switch (closest_ball.reflection.type) {
+        case NONE:
+            // no reflection nor refraction, done
+            return color;
+        case REFLECTIVE: {
+            // recursive call
+            vec3f reflected_color = trace_ray(reflection, depth - 1, T_CLOSE, t_max);
 
-    vec3f total_color = vec3f_add(
-            vec3f_scale(color, 1.f - closest_ball.reflectiveness),
-            vec3f_scale(reflected_color, closest_ball.reflectiveness)
-    );
+            vec3f total_color = vec3f_add(
+                    vec3f_scale(color, 1.f - closest_ball.reflection.fraction.reflectiveness),
+                    vec3f_scale(reflected_color, closest_ball.reflection.fraction.reflectiveness)
+            );
 
-    return total_color;
+            return total_color;
+        }
+        case REFRACTIVE: {
+            float cos_i = vec3f_dot(normal, ray.direction); // cosine of angle of incidence
+            float eta_i; // refractive index of the material the ray is in
+            float eta_t; // refractive index of the material the refractive ray is in
+
+            vec3f n; // vector pointing into the material the ray is going into
+            if (cos_i < 0) {
+                // ray hits outside of sphere
+                eta_i = 1.f;
+                eta_t = closest_ball.reflection.refractive_index;
+                cos_i = -cos_i;
+                n = normal;
+            } else {
+                // ray hits inside of sphere
+                eta_i = closest_ball.reflection.refractive_index;
+                eta_t = 1.f;
+                n = vec3f_scale(normal, -1.f);
+            }
+
+            float eta_r = eta_i / eta_t;
+            float discriminant = 1.f - eta_r * eta_r * (1.f - cos_i * cos_i);
+
+            if (discriminant < 0) {
+                // discriminant is negative, total internal refraction
+                return color;
+            }
+
+            float b = eta_r * cos_i - sqrtf(discriminant);
+            vec3f refraction_dir = vec3f_norm(vec3f_add(vec3f_scale(ray.direction, eta_r), vec3f_scale(n, b)));
+            ray_t refraction = {reflection.start, refraction_dir};
+
+            // recursive call
+            vec3f refracted_color = trace_ray(refraction, depth - 1, T_CLOSE, t_max);
+
+            vec3f total_color = vec3f_add(
+                    vec3f_scale(color, 1.f - closest_ball.reflection.fraction.refractiveness),
+                    vec3f_scale(refracted_color, closest_ball.reflection.fraction.refractiveness)
+            );
+            return total_color;
+        }
+        case REFLECTIVE_REFRACTIVE:
+            // todo reflection and refraction
+            return color;
+    }
 }
