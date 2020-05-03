25124f0de86559493fd653741af994c37dab251b
nolmoonen <nolmoonen@hotmail.nl>
2020-05-03 14:36:48 +0200

    added anti-aliasing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![splash](images/splash.png)
 
-An exercise into ray tracing containing: Blinn-Phong shading, shadows, reflections, and refractions.
+An exercise into ray tracing containing: Blinn-Phong shading, shadows, reflections, refractions, anti-aliasing.
 #### Building
 * Clone [stb](https://github.com/nothings/stb) into directory `external/stb`.
 * Build using CMake.
--- src/main.c
+++ src/main.c
@@ -44,13 +44,18 @@ const float T_MIN = 0.f;                  // default near clipping plane
 const float T_CLOSE = 0.005f;             // near clipping plane preventing sphere casting shadows and reflections on self
 const vec3f BACKGROUND = {.1f, .1f, .1f}; // color of the background
 const uint32_t DEPTH = 8;                 // number of iterations for reflections/refractions
+const uint32_t RAYS_PER_PIXEL_X = 2;                // number of pixels in horizontal direction
+const uint32_t RAYS_PER_PIXEL_Y = RAYS_PER_PIXEL_X; // number of pixels in vertical direction
+const uint32_t RAYS_PER_PIXEL = RAYS_PER_PIXEL_X * RAYS_PER_PIXEL_Y; // the level of supersampling
 
 int main()
 {
     // https://en.wikipedia.org/wiki/Ray_tracing_(graphics)
-    const uint32_t K = WIDTH;  // number of pixels in horizontal direction
-    const uint32_t M = HEIGHT; // number of pixels in vertical direction
-    const float THETA = FOV;   // field of view
+    const uint32_t SIZE_X = WIDTH;                // number of pixels in horizontal direction
+    const uint32_t SIZE_Y = HEIGHT;               // number of pixels in vertical direction
+    const uint32_t K = SIZE_X * RAYS_PER_PIXEL_X; // number of rays in horizontal direction
+    const uint32_t M = SIZE_Y * RAYS_PER_PIXEL_Y; // number of rays in vertical direction
+    const float THETA = FOV;                      // field of view
 
     /** pre-calculation for camera rays */
     vec3f t = vec3f_sub(T, E);   // look direction
@@ -71,20 +76,30 @@ int main()
     color_t *buffer = malloc(M * K * sizeof(color_t));
 
     /** begin tracing */
-    for (uint32_t j = 0; j < M; j++) {
-        for (uint32_t i = 0; i < K; i++) {
-            vec3f rij = vec3f_norm(vec3f_sub(vec3f_add(p11, vec3f_scale(qx, i)), vec3f_scale(qy, j)));
-            ray_t ray = {.start=E, .direction=rij};
+    for (uint32_t j = 0; j < SIZE_Y; j++) {
+        for (uint32_t i = 0; i < SIZE_X; i++) {
+            // color for this pixel
+            vec3f color = {0.f, 0.f, 0.f};
 
-            vec3f color = trace_ray(ray, DEPTH, T_MIN, T_MAX);
+            for (uint32_t jj = 0; jj < RAYS_PER_PIXEL_Y; jj++) {
+                for (uint32_t ii = 0; ii < RAYS_PER_PIXEL_X; ii++) {
+                    uint32_t x = i * RAYS_PER_PIXEL_X + ii;
+                    uint32_t y = j * RAYS_PER_PIXEL_Y + jj;
+                    vec3f rij = vec3f_norm(vec3f_sub(vec3f_add(p11, vec3f_scale(qx, x)), vec3f_scale(qy, y)));
+                    ray_t ray = {.start=E, .direction=rij};
+
+                    vec3f computed_color = trace_ray(ray, DEPTH, T_MIN, T_MAX);
+                    color = vec3f_add(color, vec3f_scale(computed_color, 1.f / (float) RAYS_PER_PIXEL));
+                }
+            }
 
             // apply pixel
             color_t colorRGB = {(uint8_t) (color.x * 255), (uint8_t) (color.y * 255), (uint8_t) (color.z * 255)};
-            memcpy(&buffer[j * K + i], &colorRGB, sizeof(color_t));
+            memcpy(&buffer[j * SIZE_X + i], &colorRGB, sizeof(color_t));
         }
     }
 
-    stbi_write_png("out.png", K, M, sizeof(color_t), buffer, (signed) (K * sizeof(color_t)));
+    stbi_write_png("out.png", SIZE_X, SIZE_Y, sizeof(color_t), buffer, (signed) (SIZE_X * sizeof(color_t)));
 
     free(buffer);
 
